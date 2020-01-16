e598474762993b36d246f1a8c63681841f6f88f6
Omkar Deshpande <edu.omkar@gmail.com>
2020-01-16 20:56:48 +0530

    Farmer Dashboard Layout

--- farmer_portal/farmers/urls.py
+++ farmer_portal/farmers/urls.py
@@ -4,4 +4,6 @@ from . import views as farmer_views
 urlpatterns = [
     # path('', farmer_views.dashboard , name='farmer-dashboard'),
     path('dashboard/', farmer_views.dashboard , name='farmer-dashboard'),
+    path('addProduce/', farmer_views.addProduce , name='farmer-addProduce-Form'),
+    path('check/', farmer_views.check , name='farmer-check'),
 ]
--- farmer_portal/farmers/views.py
+++ farmer_portal/farmers/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
+transactions = [
+    {
+        'name':'Omkar Deshpande',
+        'location':'Nanded',
+        'product':'Rice',
+        'qty':20,
+    },
+    {
+        'name':'Aditya Jahagirdar',
+        'location':'Aurangabad',
+        'product':'Corn',
+        'qty':30,
+    },
+    {
+        'name':'Omkar Deshpande',
+        'location':'Nanded',
+        'product':'Rice',
+        'qty':20,
+    },
+    {
+        'name':'Aditya Jahagirdar',
+        'location':'Aurangabad',
+        'product':'Corn',
+        'qty':30,
+    },
+    {
+        'name':'Omkar Deshpande',
+        'location':'Nanded',
+        'product':'Rice',
+        'qty':20,
+    },
+    {
+        'name':'Aditya Jahagirdar',
+        'location':'Aurangabad',
+        'product':'Corn',
+        'qty':30,
+    },
+]
 
 farmer_details = [
     {
@@ -11,5 +49,17 @@ farmer_details = [
 def dashboard(request):
     context={
         'details': farmer_details,
+        'transactions':transactions,
     }
     return render(request, 'farmers/dashboard.html', context)
+
+def addProduce(request):
+    context={
+        'details': farmer_details,
+    }
+    return render(request, 'farmers/add_produce.html', context)
+def check(request):
+    context={
+        'details': farmer_details,
+    }
+    return render(request, 'farmers/dashboard00.html', context)
