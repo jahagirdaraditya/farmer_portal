6b637f2839f8bed860549d1ea7cb25e4c278424f
Omkar Deshpande <edu.omkar@gmail.com>
2020-01-21 16:28:25 +0530

    Removed heads

--- farmer_portal/portal/urls.py
+++ farmer_portal/portal/urls.py
@@ -6,14 +6,8 @@ from farmers import views as farmer_views
 urlpatterns = [
     path('', portal_views.home , name='portal-home'),
     path('farmer/', include('farmers.urls')),
-<<<<<<< HEAD
     path('service/', include('services.urls')),
-=======
     path('buyer/', include('buyer.urls')),
     path('admin_portal/', include('admin_portal.urls')),
-<<<<<<< HEAD
     path('login/', portal_views.login, name='login')
-=======
->>>>>>> 41e5102c38803e61a545fd2defe14d107cc28601
->>>>>>> 4c79d4b9f607156e9777fe6895c17a1774d7332f
 ]
